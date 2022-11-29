ebbcea2660aec1e303a06d88d6d48971e40d6420
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-11-29 14:00:58 +0700

    feat: add airplane api

--- app/router.js
+++ app/router.js
@@ -6,23 +6,27 @@ const {
   ApplicationController,
   AuthenticationController,
   UserController,
-  AirportController
+  AirportController,
+  AirplaneController
 } = require("./controllers");
 const {
   User,
   Role,
-  Airport
+  Airport,
+  Airplane
 } = require("./models");
 
 function apply(app) {
   const roleModel = Role;
   const userModel = User;
   const airportModel = Airport;
+  const airplaneModel = Airplane;
 
   const applicationController = new ApplicationController();
   const authenticationController = new AuthenticationController({ bcrypt, jwt, roleModel, userModel, });
   const userController = new UserController({ userModel });
   const airportController = new AirportController({ airportModel });
+  const airplaneController = new AirplaneController({ airplaneModel });
 
   const accessControl = authenticationController.accessControl;
 
@@ -33,6 +37,12 @@ function apply(app) {
   app.get("/api/v1/airports/:id", airportController.handleGetAirport);
   app.put("/api/v1/airports/:id", airportController.handleUpdateAirport);
   app.delete("/api/v1/airports/:id", airportController.handleDeleteAirport);
+
+  app.post("/api/v1/airplanes", airplaneController.handleCreateAirplane);
+  app.get("/api/v1/airplanes/:id", airplaneController.handleGetAirplane);
+  app.put("/api/v1/airplanes/:id", airplaneController.handleUpdateAirplane);
+  app.delete("/api/v1/airplanes/:id", airplaneController.handleDeleteAirplane);
+  app.get("/api/v1/airplanes", airplaneController.handleListAirplane);
 
   app.post("/api/auth/login", authenticationController.handleLogin);
   app.post("/api/auth/register", authenticationController.handleRegister);
